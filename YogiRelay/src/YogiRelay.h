745fbbaf70245a60c2a7cda8327312df135226b0
John Griswold <john@grizzlyweb.com>
2020-08-27 11:17:47 -0500

    Allow 'confirm' circuit logic

--- YogiRelay/src/YogiRelay.h
+++ YogiRelay/src/YogiRelay.h
@@ -3,19 +3,24 @@
 #define YogiRelay_h
 
 
-// Control the macros below to enable which relay is used.
-#if ! defined( RELAY_HFD2 ) && ! defined( RELAY_SONGLE )
-#    define RELAY_SONGLE
-#endif
-
-
 //============== RELAY ===============
 
+// This class controls two types of relays.
+// - Simple relay where you set the control
+//   pin to HIGH to enable.
+// - Latching relay where there are two pins:
+//   one to 'set' and another to 'reset'
+//
+// The constructor (or the init function)
+// controls the various modes of operation.
+//
+// To use the 'confirm' functionality hook
+// the relay pin that is active (normally closed)
+// to the 'confirm' pin.  You may need to
+// put a resistor depending upon the output
+// voltage.
 
-// This class controls either
-// This class is for relays that use two pins.
-// Each pin switches the output to one of two
-// outputs.
+
 class YogiRelay
 {
 public:
@@ -24,16 +29,18 @@ public:
     YogiRelay()
             : m_bSet( false )
             , m_pinSet( 0 )
-#if defined( RELAY_HFD2 )
             , m_pinReset( 0 )
-#endif
+            , m_pinConfirm( 0 )
     {}
-    YogiRelay( uint8_t on, uint8_t off )
+
+    YogiRelay( uint8_t set,         //! < pin that controls "on"
+            uint8_t    reset = 0,   //! < if using latching relay "off"
+            uint8_t    confirm = 0  //! < extra pin to confirm "set"
+            )
             : m_bSet( false )
-            , m_pinSet( on )
-#if defined( RELAY_HFD2 )
-            , m_pinReset( off )
-#endif
+            , m_pinSet( set )
+            , m_pinReset( reset )
+            , m_pinConfirm( confirm )
     {}
 
 
@@ -41,43 +48,59 @@ public:
     // public functions ---------------
 
     void
-    init( uint8_t set = 0, uint8_t reset = 0 )
+    init( uint8_t set = 0, uint8_t reset = 0, uint8_t confirm = 0 )
     {
         if ( 0 < set )
             m_pinSet = set;
         if ( 0 < m_pinSet )
             pinMode( m_pinSet, OUTPUT );
-#if defined( RELAY_HFD2 )
         if ( 0 < reset )
             m_pinReset = reset;
+
+        // latching relay
         if ( 0 < m_pinReset )
             pinMode( m_pinReset, OUTPUT );
-#endif
+        if ( 0 < confirm )
+            m_pinConfirm = confirm;
+        // latching with confirm circuit
+        if ( 0 < m_pinConfirm )
+            pinMode( m_pinConfirm, INPUT );
         this->reset();
     }
 
+    //! set the relay such that the 'normally open'
+    //! pin is now 'closed'.
     void
     set()
     {
         m_bSet = true;
-#if defined( RELAY_HFD2 )
-        trigger( m_pinSet );
-#elif defined( RELAY_SONGLE )
-        digitalWrite( m_pinSet, HIGH );
-        delay( 400 );
-#endif
+        // are we dealing with a latching relay
+        if ( 0 < m_pinReset )
+        {
+            trigger( m_pinSet, LOW );
+        }
+        else
+        {
+            digitalWrite( m_pinSet, HIGH );
+            delay( 400 );
+        }
     }
 
+    //! reset the relay to its 'normally open'
+    //! and 'normally closed' state.
     void
     reset()
     {
         m_bSet = false;
-#if defined( RELAY_HFD2 )
-        trigger( m_pinReset );
-#elif defined( RELAY_SONGLE )
-        digitalWrite( m_pinSet, LOW );
-        delay( 400 );
-#endif
+        if ( 0 < m_pinReset )
+        {
+            trigger( m_pinReset, HIGH );
+        }
+        else
+        {
+            digitalWrite( m_pinSet, LOW );
+            delay( 400 );
+        }
     }
 
     bool
@@ -95,26 +118,39 @@ public:
 protected:
     // protected functions ------------
 
-#if defined( RELAY_HFD2 )
+    // trigger the indicated 'pin'
+    // confirm that 'reset' state is either HIGH (reset) or LOW (set)
     void
-    trigger( uint8_t pin )
+    trigger( uint8_t pin, uint8_t state )
     {
         if ( 0 < pin )
         {
-            digitalWrite( pin, HIGH );
-            delay( 5 );
-            digitalWrite( pin, LOW );
+            if ( 0 < m_pinConfirm )
+            {
+                digitalWrite( pin, HIGH );
+                for ( int i = 1; i < 20; ++i )
+                {
+                    delay( 5 );
+                    if ( state == digitalRead( m_pinConfirm ) )
+                        break;
+                }
+                digitalWrite( pin, LOW );
+            }
+            else
+            {
+                digitalWrite( m_pinSet, HIGH );
+                delay( 5 );
+                digitalWrite( m_pinSet, LOW );
+            }
         }
     }
-#endif
 
     // protected data -----------------
 
     bool    m_bSet;
     uint8_t m_pinSet;
-#if defined( RELAY_HFD2 )
     uint8_t m_pinReset;
-#endif
+    uint8_t m_pinConfirm;
 };
 
 
